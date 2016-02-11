006b15f45b7634dab9db1374add22a161eac8103
Sergey Golovin <golovim@gmail.com>
2016-02-11 14:55:11 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -3,31 +3,52 @@ CYAN='\033[0;36m'
 NORMAL='\033[0m'
 YELLOW='\033[0;33m'
 SCRIPTPATH=`pwd -P`
-echo "${SCRIPTPATH}"
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+function brew_install () {
+  if ! type "$$1" > /dev/null 2> /dev/null; then
+    echo "${GREEN}$1 already installed!${NORMAL}"
+  else
+    brew install $1
+  fi
+}
+
+echo "${CYAN}Installation started...\n${NORMAL}"
+
+if ! type "$brew" > /dev/null 2> /dev/null; then
+  echo "${GREEN}brew already installed!${NORMAL}"
+else
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
 echo "${CYAN}Brew updating...${NORMAL}"
 brew update
 echo "${GREEN}DONE!${NORMAL}"
 
 echo "${CYAN}Install apps from brew...${NORMAL}"
-brew install python
-brew install macvim --override-system-vim
-brew install tmux
-brew install neovim/neovim/neovim
-brew install curl
-brew install zsh
-brew install git
+brew_install python
+brew_install "macvim --override-system-vim"
+brew_install tmux
+
+if ! type "$nvim" > /dev/null 2> /dev/null; then
+  echo "${GREEN}neovim already installed!${NORMAL}"
+else
+  brew install neovim/neovim/neovim
+fi
+
+brew_install curl
+brew_install zsh
+brew_install git
 echo "${GREEN}DONE!${NORMAL}"
 
 echo "${CYAN}Install vim plug...${NORMAL}"
-mv ~/.config/nvim ~/.config/nvim.back
+mv --backup=numbered ~/.config/nvim ~/.config/nvim.back
 curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo "${GREEN}DONE!${NORMAL}"
 
 echo "${CYAN}Create symlinks to .tmux.conf and init.vim...${NORMAL}"
 mv ~/.tmux.conf ~/.tmux.conf.back
 ln -s ${SCRIPTPATH}/.tmux.conf ~/.tmux.conf
+mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.back
 ln -s ${SCRIPTPATH}/init.vim ~/.config/nvim/init.vim
 echo "${GREEN}DONE!${NORMAL}"
 
@@ -43,7 +64,7 @@ ln -s ${SCRIPTPATH}/.zshrc ~/.zshrc
 chsh -s $(which zsh)
 echo "${GREEN}DONE!${NORMAL}"
 echo "${GREEN}COMPLETE!${NORMAL}"
-echo "${YELLOW}NOTE: ${NORMAL} You should install Neovim plugins (:PlugInstall). But before do it you should set up your git (set your email, username and so on) and compile YouCompleteMe. \nSEE: ${CYAN}https://github.com/Valloric/YouCompleteMe#mac-os-x-installation${NORMAL}\n OR: ${CYAN}sudo sh ~/.vim/plugged/YouCompleteMe/install.sh${NORMAL}"
+echo "\n\n${YELLOW}NOTE: ${NORMAL} You should install Neovim plugins (:PlugInstall). But before do it you should set up your git (set your email, username and so on) and compile YouCompleteMe. \nSEE: ${CYAN}https://github.com/Valloric/YouCompleteMe#mac-os-x-installation${NORMAL}\n OR: ${CYAN}sudo sh ~/.vim/plugged/YouCompleteMe/install.sh${NORMAL}"
 
 
 
