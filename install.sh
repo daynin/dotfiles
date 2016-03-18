3a1f1461cd2d38948aa951c6fb88f5e70b2bd096
Sergey Golovin <golovim@gmail.com>
2016-03-19 01:23:24 +0300

    update images

--- install.sh
+++ install.sh
@@ -48,7 +48,8 @@ brew_install curl
 brew_install zsh
 brew_install git
 
-cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf<Paste>
+cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
+
 echo "${GREEN}DONE!${NORMAL}"
 
 echo "${CYAN}Install vim plug...${NORMAL}"
