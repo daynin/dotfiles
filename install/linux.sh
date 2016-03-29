1fd0c93bc13aac1693a0a90f10ef8d7ef16fc805
Sergey Golovin <golovim@gmail.com>
2016-03-29 20:46:05 +0300

    make installation on linux more quiet

--- install/linux.sh
+++ install/linux.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 printf "${CYAN}Apt-get updating...${NORMAL}\n"
 sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt-get update -q
+sudo apt-get -qq update
 printf "${GREEN}DONE!${NORMAL}\n"
 
-printf "${CYAN}Install apps from brew...${NORMAL}\n"
-sudo apt-get install python -q
-sudo apt-get install vim -q
-sudo apt-get install tmux -q
+printf "${CYAN}Install apps from apt-get...${NORMAL}\n"
+sudo apt-get -qqy install python
+sudo apt-get -qqy install vim
+sudo apt-get -qqy install tmux
 
-sudo apt-get install software-properties-common -q
-sudo apt-get install python-software-properties -q
-sudo apt-get install neovim -q
-sudo apt-get install python-dev -q
-sudo apt-get install python-pip -q
-sudo apt-get install python3-dev -q
-sudo apt-get install python3-pip -q
+sudo apt-get -qqy install software-properties-common
+sudo apt-get -qqy install python-software-properties
+sudo apt-get -qqy install neovim
+sudo apt-get -qqy install python-dev
+sudo apt-get -qqy install python-pip
+sudo apt-get -qqy install python3-dev
+sudo apt-get -qqy install python3-pip
 
-sudo apt-get install curl -q
-sudo apt-get install zsh -q
-sudo apt-get intall git -q
+sudo apt-get -qqy install curl
+sudo apt-get -qqy install zsh
+sudo apt-get -qqy intall git
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
