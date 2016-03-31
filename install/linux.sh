57ac2373736c8bcb8f5f9f151046c0220d5512ba
Sergey Golovin <golovim@gmail.com>
2016-03-31 18:21:24 +0300

    add git-extras as additional package

--- install/linux.sh
+++ install/linux.sh
@@ -19,7 +19,7 @@ sudo apt-get -qqy install python3-pip
 
 sudo apt-get -qqy install curl
 sudo apt-get -qqy install zsh
-sudo apt-get -qqy intall git
+sudo apt-get -qqy install git-extras
 
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
--- install/osx.sh
+++ install/osx.sh
@@ -29,7 +29,7 @@ fi
 
 brew_install curl
 brew_install zsh
-brew_install git
+brew_install git-extras
 
 cd ~/Library/Fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
 
