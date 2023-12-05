94599ed1dd610153e8687068c33fc1123afd8834
Sergey Golovin <sergey.golovin@protonmail.com>
2023-12-05 17:57:02 -0300

    update UI of tmux

--- tmux/.config/tmux/scripts/get-last-task.sh
+++ tmux/.config/tmux/scripts/get-last-task.sh
@@ -4,5 +4,5 @@ if task &> /dev/null;
 then
   last_task=$(task _get 1.description)
 
-  printf "#[fg=#a6d189,bold,bg=#303446] 󰱒  #[fg=#c6d0f5,bold,bg=#303446]${last_task}  "
+  printf "#[fg=#a6d189,bold,bg=#303446]   #[fg=#c6d0f5,bold,bg=#303446]${last_task}  "
 fi
--- tmux/.config/tmux/scripts/get-tasks-count.sh
+++ tmux/.config/tmux/scripts/get-tasks-count.sh
@@ -2,5 +2,5 @@
   then
     tasks_count=$(task status:pending count)
 
-    printf "#[fg=#e5c890,bold,bg=#303446]󰝖  $tasks_count"
+    printf "#[fg=#e5c890,bold,bg=#303446]  $tasks_count"
   fi
