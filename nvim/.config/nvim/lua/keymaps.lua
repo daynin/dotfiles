f3eab7f304b4d22dcdd250ac30e0ad1d8b74d9c3
Sergey Golovin <sergey.golovin@protonmail.com>
2023-12-05 21:25:31 -0300

    show shorten paths in telescope results

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -2,10 +2,7 @@ local function map(mode, key, value)
     vim.keymap.set(mode, key, value)
 end
 
-map('n', '<C-p>', ':Telescope git_files<cr>')
-map('n', '<C-f>', ':Telescope live_grep<cr>')
 map('n', '<C-n>', ':Neotree toggle<cr>')
-
 
 map('n', '<Leader>t', ':TestFile<cr>')
 map('n', '<Leader>n', ':TestNearest<cr>')
@@ -15,3 +12,10 @@ map('n', '<Leader>f', vim.lsp.buf.format)
 map('n', 'gs', vim.lsp.buf.signature_help)
 map('n', 'gh', vim.lsp.buf.hover)
 map('n', 'ga', vim.lsp.buf.code_action)
+
+local builtin = require('telescope.builtin')
+map('n', '<Leader>b', function() builtin.buffers {} end)
+map('n', '<C-p>', function() builtin.git_files { path_display={'smart'} } end)
+map('n', '<C-f>', function() builtin.live_grep { path_display={'smart'} } end)
+map('n', 'gd', function() builtin.lsp_definitions { path_display={'smart'} } end)
+map('n', 'gr', function() builtin.lsp_references { path_display={'smart'}, show_line=false } end)
--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -22,17 +22,6 @@ require('lazy').setup({
   'ziglang/zig.vim',
   'simrat39/rust-tools.nvim',
   {
-    'folke/flash.nvim',
-    event = "VeryLazy",
-    opts = {},
-    keys = {
-      { 's', mode = { 'n', 'x', 'o' }, function() require('flash').jump() end, desc = 'flash' },
-      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
-      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
-      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
-    },
-  },
-  {
     'norcalli/nvim-colorizer.lua',
     config = function()
       vim.o.termguicolors = true
